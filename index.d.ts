459e226258019dd9db4a4c91a68814e4ed5fce6c
clayton <me@clayton.cc>
2020-10-31 22:00:27 -0700

    Export Key type separately

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,18 @@
 /**
+ * Key of an object.
+ */
+export type Key = keyof any;
+
+/**
  * Dictionary mapping keys to values.
  */
-export type Dictionary<T, K extends keyof any = string> = {
+export type Dictionary<T, K extends Key = string> = {
   [P in K]?: T;
 };
 
 /**
  * Dictionary mapping keys to values or nested dictionaries.
  */
-export type DeepDictionary<T, K extends keyof any = string> = {
+export type DeepDictionary<T, K extends Key = string> = {
   [P in K]?: T | DeepDictionary<T, K>;
 };
