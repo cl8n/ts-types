61ae44133a2a2bb8c2f194731ce587fd98a12264
clayton <me@clayton.cc>
2020-11-02 01:55:54 -0800

    Add Empty type

--- index.d.ts
+++ index.d.ts
@@ -13,6 +13,29 @@ export type Dictionary<V, K extends Key = string> = {
 };
 
 /**
+ * Empty value of the given type. Types with no logical empty or default are unchanged.
+ *
+ * @example
+ * type N = Empty<number>; // 0
+ * type S = Empty<string>; // ''
+ * type A = Empty<ExampleType[]>; // []
+ * type O = Empty<{ num: number }>; // { num: 0 }
+ */
+export type Empty<T> = T extends boolean
+  ? false
+  : T extends number
+  ? 0
+  : T extends string
+  ? ''
+  : T extends unknown[]
+  ? []
+  : T extends Record<infer K, unknown>
+  ? {
+      [P in K]: Empty<T[P]>;
+    }
+  : T;
+
+/**
  * Key of an object.
  */
 export type Key = keyof any;
